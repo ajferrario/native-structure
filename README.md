d1443e66543808babbe839e0635b35dd516f5bf1
Anthony Ferrario <anthony.ferrario@gmail.com>
2017-03-05 22:53:39 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# structural components for react-native
+
+In the process of building my first React-Native (RN) app I've found that the default View components combined with the flexbox-like properties made available within RN are very confusing and tend to cost large amounts of time due to the non-intuitive nature of the way the styling properties combine. In order to solve this problem I came up with these basic components which will act as more intelligent substitutes for the View component and do a better job of handling the different structural layouts which are commonly found in apps more gracefully.
+
+### contents
+
+- [screen] #Screen
+- [column] #Column
+- [row] #Row
+- [container] #Container
+
+## screen
+
+
+## column
+
+
+## row
+
+
+## container
---
+++ styles/Style.js
@@ -0,0 +1,128 @@
+/* Module */
+import _ from 'lodash';
+
+/* Local */
+import {
+  Utility,
+} from './StyleGuide';
+
+/* Private */
+function _structure(flex, backgroundColor, flexDirection) {
+  /*
+  Prototype structural style used to build other structural styles
+  */
+  const style = {
+    flex: flex,
+    backgroundColor: backgroundColor,
+  }
+  return _.merge(style, _direction(flexDirection));
+}
+
+function _direction(flexDirection) {
+  /*
+  Provide orientation independent logical default settings
+  */
+  const directionSettings = {
+    flexDirection: flexDirection,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+  };
+  if ( flexDirection === 'column' ) {
+    directionSettings.justifyContent = 'flex-start';
+  } else if (flexDirection === 'row') {
+    directionSettings.alignItems = 'flex-start';
+  }
+  return directionSettings;
+}
+
+function _align(orientation, vAlign, hAlign) {
+  /*
+  Provide an orientation independent logical alignment setting
+  */
+  const alignment = {};
+  const vProp = (orientation === 'row'? 'justifyContent' : 'alignItems');
+  const hProp = (orientation === 'row'? 'alignItems' : 'justifyContent');
+
+  switch ( vAlign ) {
+    case 'left':
+      alignment[vProp] = 'flex-start';
+      break;
+    case 'right':
+      alignment[vProp] = 'flex-end';
+      break;
+    case 'center':
+      alignment[vProp] = 'center';
+      break;
+    default:
+      if ( vProp === 'alignItems') {
+        alignment[vProp] = 'stretch';
+      } else {
+        alignment[vProp] = 'space-around';
+      }
+  }
+
+  switch ( hAlign ) {
+    case 'left':
+      alignment[hProp] = 'flex-start';
+      break;
+    case 'right':
+      alignment[hProp] = 'flex-end';
+      break;
+    case 'center':
+      alignment[hProp] = 'center';
+      break;
+    default:
+    if ( hProp === 'alignItems') {
+      alignment[hProp] = 'stretch';
+    } else {
+      alignment[hProp] = 'space-around';
+    }
+  }
+  return alignment;
+}
+
+/* Public */
+function ScreenStyle(backgroundColor=Utility.light,flexDirection='column') {
+  /*
+  Flexible View style intended for the highest level view of any scene.
+
+  The best practice for this style is to make sure that it occupies all the
+  screen and provides the background color for the application while it's scene
+  is active.
+
+  This style should NEVER contain anything but other view objects since it will
+  not react properly to shaping.
+   */
+  return _structure(1, backgroundColor, flexDirection);
+}
+
+function ColumnStyle(flex=1, backgroundColor='', hAlign='center', vAlign='flex-start') {
+  /*
+  Flexible View style intended as a vertical subdivision of a scene.
+  */
+  const style = _structure(flex, backgroundColor, 'column');
+  return _.merge(style, _align('column', hAlign, vAlign), );
+}
+
+function RowStyle(flex=1, backgroundColor='transparent', hAlign='flex-start', vAlign='center') {
+  /*
+  Flexible View style intended as a horizontal subdivision of a scene
+  */
+  const style = _structure(flex, backgroundColor, 'row');
+  return _.merge(style, _align('row', hAlign, vAlign));
+}
+
+function ContainerStyle(flex=1, backgroundColor='transparent', hAlign='center', vAlign='center') {
+  /*
+  Flexible View style intended as a container for a single component
+  */
+  const style = _structure(flex, backgroundColor, 'center');
+  return _.merge(style, _align('center', hAlign, vAlign));
+}
+
+export {
+  ScreenStyle,
+  ColumnStyle,
+  RowStyle,
+  ContainerStyle,
+};
